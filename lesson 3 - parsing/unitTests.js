7fae85bcc168f1e51b63788660f3879cf59d51ef
Tony Aldridge <zaragopha@hotmail.com>
2012-04-23 21:35:48 +0100

    Basic Scheem PEG, unit tests for Homework part 1

---
+++ lesson 3 - parsing/unitTests.js
@@ -0,0 +1,47 @@
+// Unit tests for scheem
+
+var PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs');
+
+var parser = fs.readFileSync('scheem.peg', 'utf-8');
+
+console.log(parser);
+
+var parse = PEG.buildParser(parser).parse;
+
+// Unit tests here!
+console.log('----- Unit tests -----');
+
+// Assert single atoms work
+assert.deepEqual( parse('wugahumphdamumph'), 'wugahumphdamumph' );
+
+// Assert single item expressions work
+assert.deepEqual( parse('(1)'), ['1']);
+
+// Assert multiple item expressions work
+assert.deepEqual( parse('(* 1 2)'), ['*', '1', '2'] );
+
+// Assert nested expressions work
+assert.deepEqual( parse('(* (* 1 2) 3)'), ['*', ['*', '1', '2'], '3'] );
+
+// Assert double nested expressions work
+assert.deepEqual( parse('(* (* 1 2) (* 1 2))'), ['*', ['*', '1', '2'], ['*', '1', '2'] ] );
+
+// Assert silly amounts of whitespace
+assert.deepEqual( parse('( * (* 1    2)    (  * 1 2     ))'), ['*', ['*', '1', '2'], ['*', '1', '2'] ] );
+
+// Assert newlines and tabs
+assert.deepEqual( parse('( * \n(* 1\t2)(  * 1 2     ))'), ['*', ['*', '1', '2'], ['*', '1', '2'] ] );
+
+// Assert basic quotes
+assert.deepEqual( parse("'x"), ['quote', 'x'] );
+
+// Assert nested quotes
+assert.deepEqual( parse("(('x) ('1 2 3)"), [['quote', 'x'], ['quote', ['1', '2', '3']]] );
+
+// Assert comments
+assert.deepEqual( parse(";;lol comment\nx"), 'x' );
+
+// Assert comments with valid syntax inside
+assert.deepEqual( parse(";;lol comment (1, 2, 3)\n(x, y)"), ['x', 'y'] );

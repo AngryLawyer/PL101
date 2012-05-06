35dd4bc6287e51a8dd34b835c39089b033857e92
Tony Aldridge <zaragopha@hotmail.com>
2012-05-06 14:19:15 +0100

    Online interpreter

--- scheem/scripts/scheemOnline.js
+++ scheem/scripts/scheemOnline.js
@@ -1,5 +1,17 @@
 (function($){
     $(document).ready(function(){
         var myCodeMirror = CodeMirror.fromTextArea($('#editor')[0]);
+
+        $('#interpret').click(function(event){
+            event.preventDefault();
+            try
+            {
+                $('#output').text(evalScheemString(myCodeMirror.getValue()));
+            }
+            catch(error)
+            {
+                $('#output').text('Exception: '+error.message);
+            }
+        });
     });
 })(jQuery);
